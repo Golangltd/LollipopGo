fd645c76c3f9673752484526d69451cb13446485
cserli <1528884535@qq.com>
2018-03-02 21:45:20 +0800

    add cache

--- library/lollipop/cache/cache.go
+++ library/lollipop/cache/cache.go
@@ -1,1 +1,29 @@
 package cache
+
+import (
+	"sync"
+)
+
+var (
+	cache = make(map[string]*CacheTable)
+	mutex sync.RWMutex
+)
+
+func Cache(table string) *CacheTable {
+	mutex.RLock()
+	t, ok := cache[table]
+	mutex.RUnlock()
+
+	if !ok {
+		t = &CacheTable{
+			name:  table,
+			items: make(map[interface{}]*CacheItem),
+		}
+
+		mutex.Lock()
+		cache[table] = t
+		mutex.Unlock()
+	}
+
+	return t
+}
