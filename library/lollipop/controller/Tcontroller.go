de7c18a00c3ba135529607b5ab5f946c2ec9b165
cserli <1528884535@qq.com>
2018-03-03 21:13:20 +0800

    modfiy file

--- library/lollipop/controller/Tcontroller.go
+++ library/lollipop/controller/Tcontroller.go
@@ -12,12 +12,30 @@ import (
 
 // 控制器的结构体
 type Tcontroller struct {
-	info map[int]string
-	mu   sync.RWMutex
+	MapController map[int]string
+	mc            sync.RWMutex
 }
 
 // 申请对象
 func NewController(paras ...interface{}) (m *Tcontroller) {
+
+	return
+}
+
+// 注册路由
+func (this *Tcontroller) RegisterTcontroller() {
+
+	return
+}
+
+// 删除路由
+func (this *Tcontroller) DeleteTcontroller() {
+
+	return
+}
+
+// 修改路由
+func (this *Tcontroller) ModifyTcontroller() {
 
 	return
 }
