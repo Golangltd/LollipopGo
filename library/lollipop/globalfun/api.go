342a03c794000d747b63b097ec9cffe6eb97d94e
cserli <1528884535@qq.com>
2018-03-19 22:36:50 +0800

    sortbg

--- library/lollipop/globalfun/api.go
+++ library/lollipop/globalfun/api.go
@@ -53,3 +53,40 @@ func bubbleSort(p []int) {
 	}
 
 }
+
+// 使用
+//	length := len(list)
+//	for root := length/2 - 1; root >= 0; root-- {
+//		sort(list, root, length)
+//	} //第一次建立大顶堆
+//	for i := length - 1; i >= 1; i-- {
+//		list[0], list[i] = list[i], list[0]
+//		sort(list, 0, i)
+//	} //调整位置并建并从第一个root開始建堆.假设不明确为什么,大家多把图画几遍就应该明朗了
+//	fmt.Println(list)
+
+type Person struct {
+	FirmsName  string // 商家的名字
+	XCName     string // 现场的名字
+	GameName   string // 游戏的名字
+	OpenID     string // 谁获取了积分
+	IJiFen     uint32 // 获得的积分
+	Instertime string // 获取积分的时间
+}
+
+func sortbg(list []Person, root, length int) {
+	for {
+		child := 2*root + 1
+		if child >= length {
+			break
+		}
+		if child+1 < length && list[child].IJiFen > list[child+1].IJiFen {
+			child++ //这里重点讲一下,就是调整堆的时候,以左右孩子为节点的堆可能也须要调整
+		}
+		if list[root].IJiFen < list[child].IJiFen {
+			return
+		}
+		list[root], list[child] = list[child], list[root]
+		root = child
+	}
+}
