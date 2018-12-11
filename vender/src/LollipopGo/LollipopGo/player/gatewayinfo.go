430cf271a72e896dcc428064d4dbdb26d101ff49
cserli <1528884535@qq.com>
2018-12-11 17:38:43 +0800

    update data

--- vender/src/LollipopGo/LollipopGo/player/gatewayinfo.go
+++ vender/src/LollipopGo/LollipopGo/player/gatewayinfo.go
@@ -3,7 +3,7 @@ package player
 // 玩家列表
 type GateWayList struct {
 	ServerID        int
-	ServverName     string
+	ServerName      string
 	ServerIPAndPort string
 	State           string
 	OLPlayerNum     int // 服务器目前的玩家数量
