3dab796badd2ee09f333d19d7f7ee3e5506e0d0b
cserli <1528884535@qq.com>
2018-12-26 15:41:29 +0800

    update data

--- LollipopGo/src/LollipopGo/db/mysql/Mysql_Insert.go
+++ LollipopGo/src/LollipopGo/db/mysql/Mysql_Insert.go
@@ -1,7 +1,7 @@
 package Mysyl_DB
 
 import (
-	"LollipopGo/LollipopGo/log"
+	_ "LollipopGo/LollipopGo/log"
 	"LollipopGo/LollipopGo/player"
 	"LollipopGo/LollipopGo/util"
 	"database/sql"
@@ -31,12 +31,14 @@ func insertToDB(db *sql.DB) {
 
 //------------------------------------------------------------------------------
 // 玩家数据保存
-func (this *mysql_db) InsertPlayerST2DB(data *player.PlayerSt) (bool, int) {
+func (this *mysql_db) InsertPlayerST2DB(data *player.PlayerSt) (bool, player.PlayerSt) {
 	uid := data.UID
 	// 判断是否存在
-	if this.ReadUserInfoData(util.Int2str_LollipopGo(uid)) {
-		log.Debug("数据存在！")
-		return false, 0
+	bret, bdata := this.ReadUserInfoData(util.Int2str_LollipopGo(uid))
+	fmt.Println("数据存在bret！", bret)
+	if bret {
+		fmt.Println("数据存在！", bdata)
+		return false, bdata
 	}
 	// 获取时间戳
 	tmptime := util.GetNowUnix_LollipopGo()
@@ -48,29 +50,12 @@ func (this *mysql_db) InsertPlayerST2DB(data *player.PlayerSt) (bool, int) {
 	CheckErr(err)
 	if err != nil {
 		fmt.Println("插入数据失败")
-		return false, 0
+		return false, bdata
 	} else {
 		fmt.Println("插入数据成功：", id)
 	}
 
-	return true, int(id)
-
-	//--------------------------------------------------------------------------
-	// nowTimeStr := GetTime()
-	// stmt, err := this.STdb.Prepare("insert t_userinfo set username=?,departname=?,created=?,password=?,uid=?")
-	// CheckErr(err)
-	// res, err := stmt.Exec("test", "研发中心", nowTimeStr, "123456", uid)
-	// CheckErr(err)
-	// id, err := res.LastInsertId()
-	// CheckErr(err)
-	// if err != nil {
-	// 	fmt.Println("插入数据失败")
-	// 	return false
-	// } else {
-	// 	fmt.Println("插入数据成功：", id)
-	// }
-	// return true
-
+	return true, bdata //int(id)
 }
 
 //------------------------------------------------------------------------------
