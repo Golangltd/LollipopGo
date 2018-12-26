189ce2765d56264ffe723e88d8555df2e19ed483
cserli <1528884535@qq.com>
2018-12-26 17:31:17 +0800

    update data

--- LollipopGo/src/LollipopGo/GM_Server.go
+++ LollipopGo/src/LollipopGo/GM_Server.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	_ "fmt"
+	"fmt"
 	"net/http"
 )
 
@@ -11,47 +11,47 @@ import (
 	2 玩家等级的限制
 	3 协议处理
 */
+
 func IndexHandlerGM(w http.ResponseWriter, r *http.Request) {
+	if req.Method == "GET" {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		req.ParseForm()
+		defer func() {
+			if err := recover(); err != nil {
+				fmt.Println("%s", err)
+
+				req.Body.Close()
+			}
+		}()
+		Protocol, bProtocol := req.Form["Protocol"]
+		Protocol2, bProtocol2 := req.Form["Protocol2"]
+		if bProtocol && bProtocol2 {
+			if Protocol[0] == strconv.Itoa(Proto.G_GameGM_Proto) {
+				switch Protocol2[0] {
+				case strconv.Itoa(Proto2.W2GMS_Modify_PlayerDataProto2):
+					// DB server 获取 rpc 操作
+					//------------------------------------------------------
+					// 修改Gm数据
+					data := DB_rpc_()
+					b, _ := json.Marshal(data)
+					fmt.Fprint(w, base64.StdEncoding.EncodeToString(b))
+					//------------------------------------------------------
+					return
+				default:
+					fmt.Fprintln(w, "88902")
+					return
+				}
+			}
+			fmt.Fprintln(w, "88904")
+			return
+		}
+		// 服务器获取通信方式错误 --> 8890 + 1
+		fmt.Fprintln(w, "88901")
+		return
+	}
+
+	fmt.Fprintln(w, "请用Get 方式请求!")
+	return
 }
 
-// func IndexHandlerGM(w http.ResponseWriter, r *http.Request) {
-// 	if req.Method == "GET" {
-// 		w.Header().Set("Access-Control-Allow-Origin", "*")
-// 		req.ParseForm()
-// 		defer func() {
-// 			if err := recover(); err != nil {
-// 				fmt.Println("%s", err)
-
-// 				req.Body.Close()
-// 			}
-// 		}()
-// 		Protocol, bProtocol := req.Form["Protocol"]
-// 		Protocol2, bProtocol2 := req.Form["Protocol2"]
-// 		if bProtocol && bProtocol2 {
-// 			if Protocol[0] == strconv.Itoa(Proto.G_GameGM_Proto) {
-// 				switch Protocol2[0] {
-// 				case strconv.Itoa(Proto2.W2GMS_Modify_PlayerDataProto2):
-// 					// DB server 获取 rpc 操作
-// 					//------------------------------------------------------
-// 					// 修改Gm数据
-// 					data := DB_rpc_()
-// 					b, _ := json.Marshal(data)
-// 					fmt.Fprint(w, base64.StdEncoding.EncodeToString(b))
-// 					//------------------------------------------------------
-// 					return
-// 				default:
-// 					fmt.Fprintln(w, "88902")
-// 					return
-// 				}
-// 			}
-// 			fmt.Fprintln(w, "88904")
-// 			return
-// 		}
-// 		// 服务器获取通信方式错误 --> 8890 + 1
-// 		fmt.Fprintln(w, "88901")
-// 		return
-// 	}
-
-// 	fmt.Fprintln(w, "请用Get 方式请求!")
-// 	return
-// }
+//
