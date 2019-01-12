e3c8d32a14cd82c5f62643d47f6478b2c0c39529
cserli <1528884535@qq.com>
2019-01-12 17:41:27 +0800

    backup sql

---
+++ LollipopGo/src/LollipopGo/db/mysql_sql/t_GameInfo.sql
@@ -0,0 +1,3 @@
+INSERT INTO `t_GameInfo` VALUES (1, '五子棋', 'game.a.babaliuliu.com', 6002);
+INSERT INTO `t_GameInfo` VALUES (2, '扫雷', 'game.a.babaliuliu.com', 6001);
+INSERT INTO `t_GameInfo` VALUES (3, '斗兽棋', 'game.a.babaliuliu.com', 6003);
---
+++ LollipopGo/src/LollipopGo/db/mysql_sql/t_playeremail.sql
@@ -0,0 +1,3 @@
+INSERT INTO `t_playeremail` VALUES (1, NULL, 'admin', 'admin', 1, '1547275445', 'qwertyuio', 0, 0, NULL, 'null', '1', '1547275445');
+INSERT INTO `t_playeremail` VALUES (2, NULL, 'admin', 'admin', 1, '1547275520', 'qwertyuio', 0, 0, NULL, 'null', '0', '1547275520');
+INSERT INTO `t_playeremail` VALUES (3, '111', 'admin', 'admin', 1, '1547275527', 'qwertyuio', 0, 0, NULL, 'null', '1', '1547275527');
