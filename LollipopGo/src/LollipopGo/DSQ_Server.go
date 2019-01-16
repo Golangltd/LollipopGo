fef892154e36415892c5b2b1ec1b8308c351db76
cserli <1528884535@qq.com>
2019-01-16 09:05:46 +0800

    update data

--- LollipopGo/src/LollipopGo/DSQ_Server.go
+++ LollipopGo/src/LollipopGo/DSQ_Server.go
@@ -538,8 +538,8 @@ func CheckGameIsOver(iRoomID int, strpopenid string) bool {
 		idataA := res.Data().(*RoomPlayerDSQ).AChessNum
 		fmt.Println("已经吃掉几个A", idataA)
 		if idataA == 8 {
-			data.OpenIDA = res.Data().(*RoomPlayerDSQ).OpenIDB
-			data.OpenIDB = strpopenid
+			data.OpenIDB = res.Data().(*RoomPlayerDSQ).OpenIDB
+			data.OpenIDA = strpopenid
 			PlayerSendToServer(ConnDSQ, data)
 			return true
 		}
@@ -549,8 +549,8 @@ func CheckGameIsOver(iRoomID int, strpopenid string) bool {
 		idataA := res.Data().(*RoomPlayerDSQ).AChessNum
 		fmt.Println("已经吃掉几个A", idataA)
 		if idataA == 8 {
-			data.OpenIDA = strpopenid
-			data.OpenIDB = res.Data().(*RoomPlayerDSQ).OpenIDA
+			data.OpenIDB = strpopenid
+			data.OpenIDA = res.Data().(*RoomPlayerDSQ).OpenIDA
 			PlayerSendToServer(ConnDSQ, data)
 			return true
 		}
