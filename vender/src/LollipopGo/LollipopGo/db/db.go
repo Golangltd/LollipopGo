ef2a96b2a1238a7aebb226f7738f789cc3ca7bb2
cserli <1528884535@qq.com>
2019-01-16 11:07:37 +0800

    Update db.go

--- vender/src/LollipopGo/LollipopGo/db/db.go
+++ vender/src/LollipopGo/LollipopGo/db/db.go
@@ -1,13 +1,23 @@
 package db
 
-/*
+var (
+	MasterLoginName     string
+	MasterLoginPassword string
+	SlaveLoginName      string
+	SlaveLoginPassword  string
+	MaxOpenConns        string
+	MaxIdleConns        string
+	MasterMysql_IP      string
+	MasterMysql_Port    string
+	SlaveMysql_IP       string
+	SlaveMysql_Port     string
+)
 
- DB基础库的作用建立通用性的函数和方法
- 1. new() 获取DB的操作指针，同时需要满足配置文件的需求。
- 2. 各种方法的实现,还是通用性的方法。
- 3. destroy等函数的实现，通过指针去操作各种函数的方法。
-
-*/
-func init() {
-
-}
+var (
+	LoginName        string
+	LoginPassword    string
+	MaxOpenConns     string
+	MaxIdleConns     string
+	MasterRedis_IP   string
+	MasterRedis_Port string
+)
