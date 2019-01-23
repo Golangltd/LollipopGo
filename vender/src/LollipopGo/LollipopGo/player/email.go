f004c2f36bbf6615d5fa7197083ec7f894d268b0
cserli <1528884535@qq.com>
2019-01-23 10:25:08 +0800

    GM邮件系统定义结构

--- vender/src/LollipopGo/LollipopGo/player/email.go
+++ vender/src/LollipopGo/LollipopGo/player/email.go
@@ -25,3 +25,18 @@ type EmailST struct {
 	IsGet     bool // 是否打开过
 	ItemList  map[int]*ItemST
 }
+
+//------------------------------------------------------------------------------
+
+// GM 系统的邮件的结构
+type EmailGM struct {
+	UID        int
+	Name       string
+	OPType     int    // 操作类型 1 新增 2 编辑  3 删除
+	SendType   int    // 1 表示全服玩家  2 指定玩家发送
+	PlayerUID  string // 玩家UID
+	SendTime   string // 发送时间，符串1：表示立即发送 ，预定发送表示后面定时发送
+	Content    string
+	ItemList   string
+	EmailState int // 1 已发送 2 冻结  3 发送中（预定） 4 发送失败
+}
