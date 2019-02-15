3b5fcf0948a0fe4d84de8cd1a210825859523bc2
cserli <1528884535@qq.com>
2019-02-15 14:33:41 +0800

    clientU3D

---
+++ LollipopU3D/LollipopU3D/Assets/script/helloword.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class helloword : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnGUI()
+    {
+        GUI.skin.label.fontSize = 100;
+        GUI.Label(new Rect(10, 10, Screen.width, Screen.height), "hello world!");
+    }
+}
