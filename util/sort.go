07b825f1cc4ea7953772be030f1bf60110c99949
byteedu <1528884535@qq.com>
2020-07-24 16:03:55 +0800

    u

--- util/sort.go
+++ util/sort.go
@@ -30,3 +30,26 @@ func Sort_LollipopGo(data map[string]*conf.DSQ_Exp, iExp int) int {
 	return 0
 }
 */
+
+//  i := Minimum(1, 3, 5, 7, 9, 10, -1, 1).(int)
+func Minimum(first interface{}, rest ...interface{}) interface{} {
+	minimum := first
+
+	for _, v := range rest {
+		switch v.(type) {
+		case int:
+			if v := v.(int); v < minimum.(int) {
+				minimum = v
+			}
+		case float64:
+			if v := v.(float64); v < minimum.(float64) {
+				minimum = v
+			}
+		case string:
+			if v := v.(string); v < minimum.(string) {
+				minimum = v
+			}
+		}
+	}
+	return minimum
+}
