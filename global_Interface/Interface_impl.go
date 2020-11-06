24feb3e3b09854490c882fb02399f59a60c5c53e
byteedu <1528884535@qq.com>
2020-11-06 10:57:35 +0800

    kcp

--- global_Interface/Interface_impl.go
+++ global_Interface/Interface_impl.go
@@ -1,18 +1,19 @@
 package MsgHandleClt
 
-import 	"golang.org/x/net/websocket"
-
+/*
+ps: v 2.8.X 版本以前
 type Msg_data interface {
 	HandleCltProtocol(protocol interface{}, protocol2 interface{}, ProtocolData map[string]interface{}, Connection *websocket.Conn) interface{}
 	HandleCltProtocol2(protocol2 interface{}, ProtocolData map[string]interface{}, Connection *websocket.Conn) interface{}
 	PlayerSendMessage(senddata interface{}) int
 	CloseEOF(closeEvent interface{}) int
 }
+*/
 
-/*// v 2.9.X 以后版本
+// v 2.9.X 以后版本
 type Msg_data interface {
 	HandleCltProtocol(protocol interface{}, protocol2 interface{}, ProtocolData map[string]interface{}, Connection interface{}) interface{}
 	HandleCltProtocol2(protocol2 interface{}, ProtocolData map[string]interface{}, Connection interface{}) interface{}
 	PlayerSendMessage(senddata interface{}) int
 	CloseEOF(closeEvent interface{}) int
-}*/
+}
