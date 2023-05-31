b987218d813930bcf0d153b0a2c857e7afe8804d
lihaibin@earthonline.com <lihaibin@national-space.com>
2023-05-31 14:11:10 +0800

    增加兼容pb

--- network/initNet.go
+++ network/initNet.go
@@ -10,31 +10,31 @@ import (
 // LollipopGo 支持的网络类型
 const (
 	WebSocket = "websocket"
-	RPC = "rpc"
-	TCP = "tcp"
-	KCP = "kcp"
+	RPC       = "rpc"
+	TCP       = "tcp"
+	KCP       = "kcp"
 )
 
 // 连接服务器类型
 const (
-	CONNINIT = iota
-	ConnProxy        // ConnProxy == 1 主动连接 Proxy服务器，Proxy作为全球服或者区域服
-	StartProxy       // StartProxy == 2  Proxy服务器使用
+	CONNINIT   = iota
+	ConnProxy  // ConnProxy == 1 主动连接 Proxy服务器，Proxy作为全球服或者区域服
+	StartProxy // StartProxy == 2  Proxy服务器使用
 )
 
 // 初始化网络
-func InitNet( netty string ,netdata ...interface{}) interface{} {
+func InitNet(netty string, netdata ...interface{}) interface{} {
 	switch netty {
 	case WebSocket:
-		InitConnection(netdata[0].(*websocket.Conn))
-		return IMsg
+		InitConnectionPB(netdata[0].(*websocket.Conn))
+		return IMsgPB
 	case RPC:
-		InitConnectionRPC(netty)  // rpc 不需要返回
+		InitConnectionRPC(netty) // rpc 不需要返回
 	case KCP:
-		InitConnectionKCP(netdata[0].(*kcp.UDPSession),netdata[1].(*kcp.Listener))
+		InitConnectionKCP(netdata[0].(*kcp.UDPSession), netdata[1].(*kcp.Listener))
 		return IMsg
 	case TCP:
-		InitConnectionTCP(netdata[0].(*net.Conn),netdata[1].(*net.Listener))
+		InitConnectionTCP(netdata[0].(*net.Conn), netdata[1].(*net.Listener))
 		return IMsg
 	default:
 		glog.Info("InitNet is failed,net type is not exist!")
@@ -43,7 +43,7 @@ func InitNet( netty string ,netdata ...interface{}) interface{} {
 }
 
 // 启动网络监听
-func Start(url string,route string,conntype int,netty string)  {
+func Start(url string, route string, conntype int, netty string) {
 	switch netty {
 	case WebSocket:
 
@@ -54,8 +54,3 @@ func Start(url string,route string,conntype int,netty string)  {
 		glog.Info("InitNet is failed,net type is not exist!")
 	}
 }
-
-
-
-
-
