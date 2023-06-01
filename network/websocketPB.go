c179b2c50fc3ddf8071b775329ca8f283171566b
lihaibin@earthonline.com <lihaibin@national-space.com>
2023-06-01 20:44:48 +0800

    pb协议兼容

--- network/websocketPB.go
+++ network/websocketPB.go
@@ -167,7 +167,7 @@ func PlayerSendToProxyServerPBC(conn *websocket.Conn, senddata []byte, strOpenID
 
 	proxydata1 := &Proto_Proxy.ProxyS2C_SendData{
 		Protocol:    1,
-		Protocol2:   int32(Proto_Proxy.Proxy_P2C_SendData),
+		Protocol2:   int32(Proto_Proxy.Proxy_S2P_SendData),
 		OpenId:      strOpenID,
 		PackageData: senddata,
 	}
